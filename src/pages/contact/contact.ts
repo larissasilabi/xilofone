2d49e5e00702acc567df1ddbaccb5683c3e1d17b
Larissa <larissa@softclass.com.br>
2017-10-26 02:11:18 -0200

    musica 1 ok, ajustar async das partes

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -3,100 +3,218 @@ import { NavController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 import { AlertController } from 'ionic-angular';
+import swal from 'sweetalert2';
+
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
 
-    unpairedDevices: any;
-    pairedDevices: any;
-    gettingDevices: Boolean;
-    texto: string;
-    recebido:string;
-    constructor(private bluetoothSerial: BluetoothSerial, private alertCtrl: AlertController) {
-      bluetoothSerial.enable();
-    }
+  unpairedDevices: any;
+  pairedDevices: any;
+  gettingDevices: Boolean;
+  texto: string;
+  recebido: string;
+  constructor(private bluetoothSerial: BluetoothSerial, private alertCtrl: AlertController) {
+    bluetoothSerial.enable();
+  }
 
-    startScanning() {
-      this.pairedDevices = null;
-      this.unpairedDevices = null;
-      this.gettingDevices = true;
-      this.bluetoothSerial.discoverUnpaired().then((success) => {
-        this.unpairedDevices = success;
-        this.gettingDevices = false;
-        success.forEach(element => {
-          // alert(element.name);
-        });
-      },
-        (err) => {
-          console.log(err);
-        })
-
-      this.bluetoothSerial.list().then((success) => {
-        this.pairedDevices = success;
-      },
-        (err) => {
-
-        })
-    }
-    success = (data) => alert(data);
-    fail = (error) => alert(error);
-
-    selectDevice(address: any) {
-
-      let alert = this.alertCtrl.create({
-        title: 'Connect',
-        message: 'Do you want to connect with?',
-        buttons: [
-          {
-            text: 'Cancel',
-            role: 'cancel',
-            handler: () => {
-              console.log('Cancel clicked');
-            }
-          },
-          {
-            text: 'Connect',
-            handler: () => {
-              this.bluetoothSerial.connect(address).subscribe(this.success, this.fail);
-            }
-          }
-        ]
+  startScanning() {
+    this.pairedDevices = null;
+    this.unpairedDevices = null;
+    this.gettingDevices = true;
+    this.bluetoothSerial.discoverUnpaired().then((success) => {
+      this.unpairedDevices = success;
+      this.gettingDevices = false;
+      success.forEach(element => {
+        // alert(element.name);
       });
-      alert.present();
+    },
+      (err) => {
+        console.log(err);
+      })
 
-    }
+    this.bluetoothSerial.list().then((success) => {
+      this.pairedDevices = success;
+    },
+      (err) => {
 
-    disconnect() {
-      let alert = this.alertCtrl.create({
-        title: 'Disconnect?',
-        message: 'Do you want to Disconnect?',
-        buttons: [
-          {
-            text: 'Cancel',
-            role: 'cancel',
-            handler: () => {
-              console.log('Cancel clicked');
-            }
-          },
-          {
-            text: 'Disconnect',
-            handler: () => {
-              this.bluetoothSerial.disconnect();
-            }
+      })
+  }
+  success = (data) => alert(data);
+  fail = (error) => alert(error);
+
+  selectDevice(address: any) {
+
+    let alert = this.alertCtrl.create({
+      title: 'Connect',
+      message: 'Do you want to connect with?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
           }
-        ]
-      });
-      alert.present();
-    }
+        },
+        {
+          text: 'Connect',
+          handler: () => {
+            this.bluetoothSerial.connect(address).subscribe(this.success, this.fail);
+          }
+        }
+      ]
+    });
+    alert.present();
 
-    send(){
-        this.bluetoothSerial.write(this.texto);
-        this.bluetoothSerial.read().then((data) => {this.recebido = data});
-    }
+  }
+
+  disconnect() {
+    let alert = this.alertCtrl.create({
+      title: 'Disconnect?',
+      message: 'Do you want to Disconnect?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Disconnect',
+          handler: () => {
+            this.bluetoothSerial.disconnect();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  send() {
+    this.bluetoothSerial.write(this.texto);
+    this.bluetoothSerial.read().then((data) => { this.recebido = data });
+  }
+
+  async send1() {
+    // Musica 1 - 3 partes
+    await this.bluetoothSerial.write('M04');
+    await this.bluetoothSerial.read().then((data) => {
+      if (data == 'ERRO') {
+        // lancar uma excessao
+        swal(
+          'Oops...',
+          'Erro na conexão com o Bluetooth',
+          'error'
+        );
+      }
+    });
+
+    // PXXYZZZZN...
+    // P - parte da musica
+    // XX - numero da parte
+    // Y - seguir/repetir
+    // ZZZZ - quantidade de pares (nota,tempo)
+    // N - pares (nota,tempo)
+
+    // DÓ (1), RÉ (1), MI (1), DÓ (1) 2x
+    await this.bluetoothSerial.write('P01S0008C10D10E10C10C10D10E10C10');
+    await this.bluetoothSerial.read().then((data) => {
+      if (data == 'ERRO') {
+        // lancar uma excessao
+        swal(
+          'Oops...',
+          'Erro na conexão com o Bluetooth',
+          'error'
+        );
+      }
+    });
+    await this.bluetoothSerial.read().then((data) => {
+      if (data == 'E') {
+        swal(
+          'Você perdeu...',
+          'Não foi dessa vez...Tente novamente!',
+          'error'
+        );
+      }
+    });
+
+    // MI (1), FÁ (1), SOL (2) 2x
+    await this.bluetoothSerial.write('P020006E10F10G20E10F10G20');
+    await this.bluetoothSerial.read().then((data) => {
+      if (data == 'ERRO') {
+        // lancar uma excessao
+        swal(
+          'Oops...',
+          'Erro na conexão com o Bluetooth',
+          'error'
+        );
+      }
+    });
+    await this.bluetoothSerial.read().then((data) => {
+      if (data == 'E') {
+        swal(
+          'Você perdeu...',
+          'Ah que pena! Vamos lá, tente novamente!',
+          'error'
+        );
+        // sair
+      }
+    });
+
+    //  SOL (1/2), LÁ (1/2), SOL (1/2), FÁ (1/2), MI (1), DÓ (1) 2x
+    await this.bluetoothSerial.write('P030012G05A05G05F05E10C10G05A05G05F05E10C10');
+    await this.bluetoothSerial.read().then((data) => {
+      if (data == 'ERRO') {
+        // lancar uma excessao
+        swal(
+          'Oops...',
+          'Erro na conexão com o Bluetooth',
+          'error'
+        );
+      }
+    });
+    await this.bluetoothSerial.read().then((data) => {
+      if (data == 'E') {
+        swal(
+          'Você perdeu...',
+          'Só mais um poquinho de atenção, você consegue!',
+          'error'
+        );
+        // sair
+      }
+    });
+
+    //  DÓ (1), DÓ (1), DÓ (1) 2x
+    await this.bluetoothSerial.write('P040006C10C10C10C10C10C10');
+    await this.bluetoothSerial.read().then((data) => {
+      if (data == 'ERRO') {
+        // lancar uma excessao
+        swal(
+          'Oops...',
+          'Erro na conexão com o Bluetooth',
+          'error'
+        );
+      }
+    });
+    await this.bluetoothSerial.read().then((data) => {
+      if (data == 'E') {
+        swal(
+          'Você perdeu...',
+          'Quase lá, você estava tão perto! Não desista!',
+          'error'
+        );
+      }
+    });
+
+    await swal(
+      'Parabéns !',
+      'Você ganhou!!!',
+      'success'
+    );
+  }
 
 }
-
-
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,36 +1,16 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Events } from 'ionic-angular';
 import { NativeAudio } from '@ionic-native/native-audio';
 import { trigger, state, style, transition, animate } from '@angular/animations'
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html',
-  animations: [
-    trigger('fade', [
-      state('active', style({
-        opacity: 1
-      })),
-      state('inactive', style({
-        opacity: 0.4
-      })),
-      transition('active => inactive', animate('.0.3s')),
-      transition('inactive => active', animate('.0.3s')),
-    ]),
-  ]
+  templateUrl: 'home.html'
 })
 export class HomePage {
 
-  controlRed = 'inactive';
-  controlOrange = 'inactive';
-  controlYellow = 'inactive';
-  controlGreen = 'inactive';
-  controlLightBlue = 'inactive';
-  controlDarkBlue = 'inactive';
-  controlPurple = 'inactive';
-  controlPink = 'inactive';
-
-  constructor(public navCtrl: NavController, private nativeAudio: NativeAudio) {
+  constructor(public navCtrl: NavController, private nativeAudio: NativeAudio, public events: Events) {
 
   }
 
@@ -41,55 +21,57 @@ export class HomePage {
   playC() {
     this.nativeAudio.preloadSimple('red', 'assets/audio/red.wav');
     this.nativeAudio.play('red').then(this.onSuccessPlaying, this.onError);
+    this.events.publish('button:click', "C");
+
   }
 
   // D (Ré)
   playD() {
     this.nativeAudio.preloadSimple('orange', 'assets/audio/orange.wav');
     this.nativeAudio.play('orange').then(this.onSuccessPlaying, this.onError);
+    this.events.publish('button:click', "D");
   }
 
   // E (Mi)
   playE() {
-    this.controlYellow = (this.controlYellow == 'active') ? 'inactive' : 'active';
     this.nativeAudio.preloadSimple('yellow', 'assets/audio/yellow.wav');
     this.nativeAudio.play('yellow').then(this.onSuccessPlaying, this.onError);
-
+    this.events.publish('button:click', "E");
   }
 
   // F (Fá)
   playF() {
-    this.controlGreen = (this.controlGreen == 'active') ? 'inactive' : 'active';
     this.nativeAudio.preloadSimple('green', 'assets/audio/green.wav');
     this.nativeAudio.play('green').then(this.onSuccessPlaying, this.onError);
+    this.events.publish('button:click', "F");
   }
 
   // G (Sol)
   playG() {
-    this.controlLightBlue = (this.controlLightBlue == 'active') ? 'inactive' : 'active';
     this.nativeAudio.preloadSimple('ligthblue', 'assets/audio/ligthblue.wav');
     this.nativeAudio.play('lightblue').then(this.onSuccessPlaying, this.onError);
+    this.events.publish('button:click', "G");
   }
 
   // A (Lá)
   playA() {
-    this.controlDarkBlue = (this.controlDarkBlue == 'active') ? 'inactive' : 'active';
     this.nativeAudio.preloadSimple('darkblue', 'assets/audio/darkblue.wav');
     this.nativeAudio.play('darkblue').then(this.onSuccessPlaying, this.onError);
+    this.events.publish('button:click', "A");
   }
 
   // B (Si)
   playB() {
-    this.controlPurple = (this.controlPurple == 'active') ? 'inactive' : 'active';
     this.nativeAudio.preloadSimple('purple', 'assets/audio/purple.wav');
     this.nativeAudio.play('purple').then(this.onSuccessPlaying, this.onError);
+    this.events.publish('button:click', "B");
   }
 
   // C (Dó)
   playC2() {
-    this.controlPink = (this.controlPink == 'active') ? 'inactive' : 'active';
     this.nativeAudio.preloadSimple('pink', 'assets/audio/pink.wav');
     this.nativeAudio.play('pink').then(this.onSuccessPlaying, this.onError);
+    this.events.publish('button:click', "C2");
   }
 
   onError = (data) => {
@@ -100,98 +82,124 @@ export class HomePage {
     console.log('onSuccessPlaying', data);
   }
 
-  disable() {
-    this.controlRed = 'inactive';
-    this.controlOrange = 'inactive';
-    this.controlYellow = 'inactive';
-    this.controlGreen = 'inactive';
-    this.controlLightBlue = 'inactive';
-    this.controlDarkBlue = 'inactive';
-    this.controlPurple = 'inactive';
-    this.controlPink = 'inactive';
+  async play1() {
+    await this.playM1P1();    
+    const p1Ok = await this.validaM1(1);
+    if (p1Ok) {
+      await this.playM1P2();
+      const p2OK = await this.validaM1(2);
+      if (p2OK) {
+        await this.playM1P3();
+        const p3OK = await this.validaM1(3);
+        if (p3OK) {
+          await this.playM1P4();
+          const p4OK = await this.validaM1(4);
+          if (p4OK) {
+            await swal('Parabéns!','Você completou o desafio!','success');
+          }
+        }
+      }
+    }    
+  }
+  
+  async playM1P1() {
+    // DÓ (1), RÉ (1), MI (1), DÓ (1)
+    await this.click("C");//1
+    await this.wait(1000);
+    await this.click("D");//2
+    await this.wait(1000);
+    await this.click("E");//3
+    await this.wait(1000);
+    await this.click("C");//4
+    await this.wait(2000);
+
+    //   // DÓ (1), RÉ (1), MI (1), DÓ (1)
+    await this.click("C");//5
+    await this.wait(1000);
+    await this.click("D");//6
+    await this.wait(1000);
+    await this.click("E");//7
+    await this.wait(1000);
+    await this.click("C");//8
+    await this.wait(1000);
+    console.log("fim primeira parte");
   }
 
-  async play1() {
-    // DÓ (1), RÉ (1), MI (1), DÓ (1)
-    await this.click("C").then(() =>  this.playC());
+  async playM1P2() {
+    //  MI (1), FÁ (1), SOL (2)
+    await this.playE();
     await this.wait(1000);
-    await this.click("D").then(() =>  this.playD());
+    await this.playF();
     await this.wait(1000);
-    await this.click("E").then(() =>  this.playE());
+    await this.playG();
+    await this.wait(2000);
+
+    // MI (1), FÁ (1), SOL (2)
+    await this.playE();
     await this.wait(1000);
-    await this.click("C").then(() =>  this.playC());
+    await this.playF();
+    await this.wait(1000);
+    await this.playG();
+    await this.wait(2000);
+
+    console.log("fim segunda parte");
+
+    await this.validaM1(2);
+  }
+
+  async playM1P3(){
+    // SOL (1/2), LÁ (1/2), SOL (1/2), FÁ (1/2), MI (1), DÓ (1)
+    await this.playG();
+    await this.wait(500);
+    await this.playA();
+    await this.wait(500);
+    await this.playG();
+    await this.wait(500);
+    await this.playF();
+    await this.wait(500);
+    await this.playE();
+    await this.wait(1000);
+    await this.playC();
     await this.wait(1000);
 
-  //   // DÓ (1), RÉ (1), MI (1), DÓ (1)
-  //   this.playC();
-  //   this.wait(1000);
-  //   this.playD();
-  //   this.wait(1000);
-  //   this.playE();
-  //   this.wait(1000);
-  //   this.playC();
+    // SOL (1/2), LÁ (1/2), SOL (1/2), FÁ (1/2), MI (1), DÓ (1)
+    await this.playG();
+    await this.wait(500);
+    await this.playA();
+    await this.wait(500);
+    await this.playG();
+    await this.wait(500);
+    await this.playF();
+    await this.wait(500);
+    await this.playE();
+    await this.wait(1000);
+    await this.playC();
+    await this.wait(1000);
 
-  //   // MI (1), FÁ (1), SOL (2)
-  //   this.playE();
-  //   this.wait(1000);
-  //   this.playF();
-  //   this.wait(1000);
-  //   this.playG();
-  //   this.wait(2000);
+    console.log("fim terceira parte");
+    
+    await this.validaM1(3);
+  }
 
-  //   // MI (1), FÁ (1), SOL (2)
-  //   this.playE();
-  //   this.wait(1000);
-  //   this.playF();
-  //   this.wait(1000);
-  //   this.playG();
-  //   this.wait(2000);
+  async playM1P4(){
+    // DÓ (1), DÓ (1), DÓ (1)
+    await this.playC();
+    await this.wait(1000);
+    await this.playC();
+    await this.wait(1000);
+    await this.playC();
+    await this.wait(1000);
 
-  //   // SOL (1/2), LÁ (1/2), SOL (1/2), FÁ (1/2), MI (1), DÓ (1)
-  //   this.playG();
-  //   this.wait(500);
-  //   this.playA();
-  //   this.wait(500);
-  //   this.playG();
-  //   this.wait(500);
-  //   this.playF();
-  //   this.wait(500);
-  //   this.playE();
-  //   this.wait(1000);
-  //   this.playC();
-  //   this.wait(1000);
+    // DÓ (1), DÓ (1), DÓ (1)
+    await this.playC();
+    await this.wait(1000);
+    await this.playC();
+    await this.wait(1000);
+    await this.playC();
+    await this.wait(1000);
 
-  //   // SOL (1/2), LÁ (1/2), SOL (1/2), FÁ (1/2), MI (1), DÓ (1)
-  //   this.playG();
-  //   this.wait(500);
-  //   this.playA();
-  //   this.wait(500);
-  //   this.playG();
-  //   this.wait(500);
-  //   this.playF();
-  //   this.wait(500);
-  //   this.playE();
-  //   this.wait(1000);
-  //   this.playC();
-  //   this.wait(1000);
-
-  //   // DÓ (1), DÓ (1), DÓ (1)
-  //   this.playC();
-  //   this.wait(1000);
-  //   this.playC();
-  //   this.wait(1000);
-  //   this.playC();
-  //   this.wait(1000);
-
-  //   // DÓ (1), DÓ (1), DÓ (1)
-  //   this.playC();
-  //   this.wait(1000);
-  //   this.playC();
-  //   this.wait(1000);
-  //   this.playC();
-  //   this.wait(1000);
-
-   }
+    await this.validaM1(4);
+  }
 
   play2() {
     this.playC();
@@ -202,14 +210,14 @@ export class HomePage {
   }
 
   play3() {
-    this.click("C");
-    this.click("D");
-    this.click("E");
-    this.click("F");
-    this.click("G");
-    this.click("A");
-    this.click("B");
-    this.click("C2");
+    // this.click("C");
+    // this.click("D");
+    // this.click("E");
+    // this.click("F");
+    // this.click("G");
+    // this.click("A");
+    // this.click("B");
+    // this.click("C2");
   }
 
   wait(ms) {
@@ -220,11 +228,140 @@ export class HomePage {
     }
   }
 
-  async click(button) {
-    document.getElementById(button).className += "click";
-    setInterval(() => {
-      document.getElementById(button).className = document.getElementById(button).className.replace("click", "");
-    }, 500);
+  click(button) {
+    return new Promise(resolve => {
+      document.getElementById(button).className += "click";
+      setTimeout(() => {
+        document.getElementById(button).className = document.getElementById(button).className.replace("click", "");
+        resolve();
+      }, 1000, 1);
+    });
   }
 
+  validaM1(parte): Boolean {
+    console.log(parte);
+    let ok = true;
+    let sequencia = 1;
+    switch (parte) {
+      case 1:
+        swal('Sua vez!', 'Repita a sequencia!', 'warning');
+        // C, D, E, C
+        this.events.subscribe('button:click', (id) => {
+          console.log(id);
+          if (sequencia === 1 || sequencia === 4 || sequencia === 5 || sequencia === 8) {
+            if (id === "C") {
+               sequencia++;
+            } else {
+              swal('Errado!', 'Vamos tentar novamente!', 'error');
+              ok = false;              
+            }
+          } else if (sequencia === 2 || sequencia === 6) {
+            if (id === "D") {
+              sequencia++;
+            } else {
+              swal('Errado!', 'Vamos tentar novamente!', 'error');
+              ok = false;
+            }
+          } else if (sequencia === 3 || sequencia === 7) {
+            if (id === "E") {
+              sequencia++;
+            } else {
+              swal('Errado!', 'Vamos tentar novamente!', 'error');
+              ok = false;
+            }
+          }
+          this.events.unsubscribe('button:click');
+        });
+
+
+      case 2:
+        // E, F, G
+        this.events.subscribe('button:click', (id) => {
+          console.log(id);
+          if (sequencia === 1 || sequencia === 4) {
+            if (id === "E") {
+              sequencia++;
+            } else {
+              swal('Errado!', 'Vamos tentar novamente!', 'error');
+              ok = false;
+            }
+          } else if (sequencia === 2 || sequencia === 5) {
+            if (id === "F") {
+              sequencia++;
+            } else {
+              swal('Errado!', 'Vamos tentar novamente!', 'error');
+              ok = false;
+            }
+          } else if (sequencia === 3 || sequencia === 6) {
+            if (id === "G") {
+              sequencia++;
+            } else {
+              swal('Errado!', 'Vamos tentar novamente!', 'error');
+              ok = false;
+            }
+          }
+          this.events.unsubscribe('button:click');
+        });
+
+      case 3:
+        // G, A, G, F, E, C        
+        this.events.subscribe('button:click', (id) => {
+          console.log(id);
+          if (sequencia === 1 || sequencia === 3 || sequencia === 7 || sequencia === 9) {
+            if (id === "G") {
+              sequencia++;
+            } else {
+              swal('Errado!', 'Vamos tentar novamente!', 'error');
+              ok = false;
+            }
+          } else if (sequencia === 2 || sequencia === 8) {
+            if (id === "A") {
+              sequencia++;
+            } else {
+              swal('Errado!', 'Vamos tentar novamente!', 'error');
+              ok = false;
+            }
+          } else if (sequencia === 4 || sequencia === 10) {
+            if (id === "F") {
+              sequencia++;
+            } else {
+              swal('Errado!', 'Vamos tentar novamente!', 'error');
+              ok = false;
+            }
+          } else if (sequencia === 5 || sequencia === 11) {
+            if (id === "E") {
+              sequencia++;
+            } else {
+              swal('Errado!', 'Vamos tentar novamente!', 'error');
+              ok = false;
+            }
+          } else if (sequencia === 6 || sequencia === 12) {
+            if (id === "C") {
+              sequencia++;
+            } else {
+              swal('Errado!', 'Vamos tentar novamente!', 'error');
+              ok = false;
+            }
+          }
+          this.events.unsubscribe('button:click');
+        });
+
+      case 4:
+        // G, A, G, F, E, C        
+        this.events.subscribe('button:click', (id) => {
+          console.log(id);
+          if (sequencia >= 1 && sequencia <= 6) {
+            if (id === "C") {
+              sequencia++;
+            } else {
+              swal('Errado!', 'Vamos tentar novamente!', 'error');
+              ok = false;
+            }
+          }
+          this.events.unsubscribe('button:click');
+        });
+
+      return ok;
+    }
+  }
 }

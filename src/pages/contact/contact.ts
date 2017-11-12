15832724f2e02c935c99d76902e67cdc1c8e63db
Larissa <larissa@softclass.com.br>
2017-11-12 18:18:02 -0200

    fix bugs

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,7 +6,7 @@ import { AlertController } from 'ionic-angular';
 import swal from 'sweetalert2';
 
 @Component({
-  selector: 'page-contact',
+  selector: 'page-contact',  
   templateUrl: 'contact.html'
 })
 export class ContactPage {
@@ -98,28 +98,15 @@ export class ContactPage {
   }
 
   async send1() {
-    // Musica 1 - 3 partes
-    await this.bluetoothSerial.write('M04');
-    await this.bluetoothSerial.read().then((data) => {
-      if (data == 'ERRO') {
-        // lancar uma excessao
-        swal(
-          'Oops...',
-          'Erro na conexão com o Bluetooth',
-          'error'
-        );
-      }
-    });
-
-    // PXXYZZZZN...
-    // P - parte da musica
+    
+    // MXXYZZZZN...
     // XX - numero da parte
     // Y - seguir/repetir
     // ZZZZ - quantidade de pares (nota,tempo)
     // N - pares (nota,tempo)
 
     // DÓ (1), RÉ (1), MI (1), DÓ (1) 2x
-    await this.bluetoothSerial.write('P01S0008C10D10E10C10C10D10E10C10');
+    await this.bluetoothSerial.write('MS0008C10D10E10C10C10D10E10C10');
     await this.bluetoothSerial.read().then((data) => {
       if (data == 'ERRO') {
         // lancar uma excessao
@@ -141,7 +128,7 @@ export class ContactPage {
     });
 
     // MI (1), FÁ (1), SOL (2) 2x
-    await this.bluetoothSerial.write('P020006E10F10G20E10F10G20');
+    await this.bluetoothSerial.write('MS0006E10F10G20E10F10G20');
     await this.bluetoothSerial.read().then((data) => {
       if (data == 'ERRO') {
         // lancar uma excessao
@@ -163,8 +150,8 @@ export class ContactPage {
       }
     });
 
-    //  SOL (1/2), LÁ (1/2), SOL (1/2), FÁ (1/2), MI (1), DÓ (1) 2x
-    await this.bluetoothSerial.write('P030012G05A05G05F05E10C10G05A05G05F05E10C10');
+    //  SOL (1/2), LÁ (1/2), SI (1/2), FÁ (1/2), MI (1), DÓ (1) 2x
+    await this.bluetoothSerial.write('MS0012G05A05B05F05E10C10G05A05B05F05E10C10');
     await this.bluetoothSerial.read().then((data) => {
       if (data == 'ERRO') {
         // lancar uma excessao
@@ -186,8 +173,8 @@ export class ContactPage {
       }
     });
 
-    //  DÓ (1), DÓ (1), DÓ (1) 2x
-    await this.bluetoothSerial.write('P040006C10C10C10C10C10C10');
+    //  DÓ (1), SI (1), C2 (1) 2x
+    await this.bluetoothSerial.write('MS0006C10B10C210C10B10C210');
     await this.bluetoothSerial.read().then((data) => {
       if (data == 'ERRO') {
         // lancar uma excessao
@@ -214,6 +201,16 @@ export class ContactPage {
       'success'
     );
   }
+
+  click(button) {
+    return new Promise(resolve => {
+      document.getElementById(button).className += " activated";
+      setTimeout(() => {
+        document.getElementById(button).className = document.getElementById(button).className.replace("activated", "");
+        resolve();
+      }, 100, 1);
+    });
+  }   
 
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,6 +10,9 @@ import swal from 'sweetalert2';
 })
 export class HomePage {
 
+  ok: Boolean;
+  playing: Boolean;
+
   constructor(public navCtrl: NavController, private nativeAudio: NativeAudio, public events: Events) {
 
   }
@@ -48,7 +51,7 @@ export class HomePage {
   // G (Sol)
   playG() {
     this.nativeAudio.preloadSimple('ligthblue', 'assets/audio/ligthblue.wav');
-    this.nativeAudio.play('lightblue').then(this.onSuccessPlaying, this.onError);
+    this.nativeAudio.play('ligthblue').then(this.onSuccessPlaying, this.onError);
     this.events.publish('button:click', "G");
   }
 
@@ -81,22 +84,36 @@ export class HomePage {
     console.log('onSuccessPlaying', data);
   }
 
+  play() {
+    if (!this.playing) {
+      this.playing = true;
+      this.play1();
+    }
+  }
+
+  exit() {
+    swal('Errado!', 'Vamos tentar novamente!', 'error');
+    this.ok = false;
+    this.events.unsubscribe('button:click');
+    this.playing = false;
+  }
+
   async play1() {
     await this.playM1P1();
     await swal('Sua vez!', 'Repita a sequencia!', 'warning');
-    await this.validaM1(1, 8);
-    //await this.playM1P2();
-    // await swal('Sua vez!', 'Repita a sequencia!', 'warning');
-    // await this.validaM1(2, 6);
-    // await swal('Parabéns!', 'Vamos para a terceira fase!', 'success');
-    // await this.playM1P3();
-    // await swal('Sua vez!', 'Repita a sequencia!', 'warning');
-    // await this.validaM1(3, 12);
-    // await swal('Parabéns!', 'Vamos para a última fase!', 'success');
-    // await this.playM1P4();
-    // await swal('Sua vez!', 'Repita a sequencia!', 'warning');
-    // await this.validaM1(4, 6);
-    // await swal('Parabéns!', 'Você ganhou!', 'success');
+    await this.validaM1P1();
+
+    await this.events.subscribe('go:level', (id) => {
+      console.log("level id" + id);
+      if (id === 2) {
+        this.playM1P2();
+      } else if (id === 3) {
+        this.playM1P3();
+      } else if (id === 4) {
+        this.playM1P4();
+      }
+    });
+
   }
 
   async playM1P1() {
@@ -107,8 +124,8 @@ export class HomePage {
     await this.wait(1000);
     await this.click("E");//3
     await this.wait(1000);
-    await this.click("C");//4
-    await this.wait(2000);
+    await this.click("C2");//4
+    await this.wait(1000);
 
     //   // DÓ (1), RÉ (1), MI (1), DÓ (1)
     await this.click("C");//5
@@ -117,257 +134,271 @@ export class HomePage {
     await this.wait(1000);
     await this.click("E");//7
     await this.wait(1000);
-    await this.click("C");//8
+    await this.click("C2");//8
     await this.wait(1000);
-    console.log("fim primeira parte");
   }
 
   async playM1P2() {
     //  MI (1), FÁ (1), SOL (2)
-    await this.playE();
+    await this.click("E");
     await this.wait(1000);
-    await this.playF();
+    await this.click("F")
     await this.wait(1000);
-    await this.playG();
+    await this.click("G")
     await this.wait(2000);
 
     // MI (1), FÁ (1), SOL (2)
-    await this.playE();
+    await this.click("E")
     await this.wait(1000);
-    await this.playF();
+    await this.click("F")
     await this.wait(1000);
-    await this.playG();
+    await this.click("G")
     await this.wait(2000);
 
-    console.log("fim segunda parte");
+    await swal('Sua vez!', 'Repita a sequencia!', 'warning');
 
-    await this.validaM1(2, 6);
+    await this.validaM1P2();
   }
 
   async playM1P3() {
-    // SOL (1/2), LÁ (1/2), SOL (1/2), FÁ (1/2), MI (1), DÓ (1)
-    await this.playG();
-    await this.wait(500);
-    await this.playA();
-    await this.wait(500);
-    await this.playG();
-    await this.wait(500);
-    await this.playF();
-    await this.wait(500);
-    await this.playE();
+    // SOL (1/2), LÁ (1/2), SI (1/2), FÁ (1/2), MI (1), DÓ (1)
     await this.wait(1000);
-    await this.playC();
+    await this.click("G");
     await this.wait(1000);
+    await this.click("A");
+    await this.wait(1000);
+    await this.click("B");
+    await this.wait(1000);
+    await this.click("F");
+    await this.wait(1000);
+    await this.click("E");
+    await this.wait(1000);
+    await this.click("C");
+    await this.wait(2000);
 
-    // SOL (1/2), LÁ (1/2), SOL (1/2), FÁ (1/2), MI (1), DÓ (1)
-    await this.playG();
-    await this.wait(500);
-    await this.playA();
-    await this.wait(500);
-    await this.playG();
-    await this.wait(500);
-    await this.playF();
-    await this.wait(500);
-    await this.playE();
+    // SOL (1/2), LÁ (1/2), SI (1/2), FÁ (1/2), MI (1), DÓ (1)
     await this.wait(1000);
-    await this.playC();
+    await this.click("G");
     await this.wait(1000);
+    await this.click("A");
+    await this.wait(1000);
+    await this.click("B");
+    await this.wait(1000);
+    await this.click("F");
+    await this.wait(1000);
+    await this.click("E");
+    await this.wait(1000);
+    await this.click("C");
+    await this.wait(2000);
 
-    console.log("fim terceira parte");
-
-    await this.validaM1(3, 12);
+    await swal('Sua vez!', 'Repita a sequencia!', 'warning');
+    await this.validaM1P3();
   }
 
   async playM1P4() {
     // DÓ (1), DÓ (1), DÓ (1)
-    await this.playC();
+    await this.click("C");
     await this.wait(1000);
-    await this.playC();
+    await this.click("B");
     await this.wait(1000);
-    await this.playC();
+    await this.click("C2");
     await this.wait(1000);
 
     // DÓ (1), DÓ (1), DÓ (1)
-    await this.playC();
+    await this.click("C");
     await this.wait(1000);
-    await this.playC();
+    await this.click("B");
     await this.wait(1000);
-    await this.playC();
+    await this.click("C2");
     await this.wait(1000);
 
-    await this.validaM1(4, 6);
+    await swal('Sua vez!', 'Repita a sequencia!', 'warning');
+    await this.validaM1P4();
   }
 
-  play2() {
-    this.playC();
-    this.wait(500);
-    this.playC2();
-    this.wait(500);
-    this.playC2();
+  async callLevel(musica, level) {
+    switch (musica) {
+      case 1:
+        await swal('Parabéns!', 'Vamos para a próxima fase!', 'success');
+        console.log("level " + level);
+        await this.events.unsubscribe('button:click');
+        await this.events.publish("go:level", level);
+        break;
+    }
   }
 
-  play3() {
-    // this.click("C");
-    // this.click("D");
-    // this.click("E");
-    // this.click("F");
-    // this.click("G");
-    // this.click("A");
-    // this.click("B");
-    // this.click("C2");
-  }
 
   wait(ms) {
-    var start = new Date().getTime();
-    var end = start;
-    while (end < start + ms) {
-      end = new Date().getTime();
-    }
+    return new Promise(resolve => {
+      var start = new Date().getTime();
+      var end = start;
+      while (end < start + ms) {
+        end = new Date().getTime();
+      }
+      resolve();
+    });
   }
 
   click(button) {
     return new Promise(resolve => {
+      document.getElementById(button).className = document.getElementById(button).className.replace(new RegExp("click","g"), "");
       document.getElementById(button).className += "click";
       setTimeout(() => {
-        document.getElementById(button).className = document.getElementById(button).className.replace("click", "");
+        document.getElementById(button).className = document.getElementById(button).className.replace(new RegExp("click","g"), "");
         resolve();
-      }, 500, 1);
+      }, 10, 1);
     });
   }
 
-  async validaM1(parte, tamanho) {
-    console.log("parte : " + parte);
-    console.log("tamanho : " + tamanho);
-    let ok = true;
+  async validaM1P1() {
     let sequencia = 1;
+    console.log("sequencia : " + sequencia);
     await this.events.subscribe('button:click', (id) => {
-      switch (parte) {
-        case 1:
-          // C, D, E, C
-          console.log(id);
-          if (sequencia === 1 || sequencia === 4 || sequencia === 5 || sequencia === 8) {
-            if (id === "C") {
-              sequencia++;
-              console.log(sequencia);
-            } else {
-              swal('Errado!', 'Vamos tentar novamente!', 'error');
-              ok = false;
-            }
-          } else if (sequencia === 2 || sequencia === 6) {
-            if (id === "D") {
-              sequencia++;
-              console.log(sequencia);
-            } else {
-              swal('Errado!', 'Vamos tentar novamente!', 'error');
-              ok = false;
-            }
-          } else if (sequencia === 3 || sequencia === 7) {
-            if (id === "E") {
-              sequencia++;
-              console.log(sequencia);
-            } else {
-              swal('Errado!', 'Vamos tentar novamente!', 'error');
-              ok = false;
-            }
-          }
-          if (sequencia > tamanho) {
-            swal('Parabéns!', 'Vamos para a segunda fase!', 'success');
-            console.log("unsubscribe");
-            this.events.unsubscribe('button:click');
-          }
+      // C, D, E, C
+      console.log(id);
+      if (sequencia === 1 || sequencia === 5) {
+        if (id === "C") {
+          sequencia++;
+          console.log(sequencia);
+        } else {
+          this.exit();
+        }
+      } else if (sequencia === 2 || sequencia === 6) {
+        if (id === "D") {
+          sequencia++;
+          console.log(sequencia);
+        } else {
+          this.exit();
+        }
+      } else if (sequencia === 3 || sequencia === 7) {
+        if (id === "E") {
+          sequencia++;
+          console.log(sequencia);
+        } else {
+          this.exit();
+        }
+      } else if (sequencia === 4 || sequencia === 8) {
+        if (id === "C2") {
+          sequencia++;
+          console.log(sequencia);
+        } else {
+          this.exit();
+        }
+      }
+      if (sequencia > 8) {
+        this.callLevel(1, 2);
+      }
+    });
+  }
 
-          break;
 
+  async validaM1P2() {
+    let sequencia = 1;
+    console.log("sequencia : " + sequencia);
+    await this.events.subscribe('button:click', (id) => {
+      console.log("validaM1P2")
+      // E, F, G
+      if (sequencia === 1 || sequencia === 4) {
+        if (id === "E") {
+          sequencia++;
+        } else {
+          this.exit();
+        }
+      } else if (sequencia === 2 || sequencia === 5) {
+        if (id === "F") {
+          sequencia++;
+        } else {
+          this.exit();
+        }
+      } else if (sequencia === 3 || sequencia === 6) {
+        if (id === "G") {
+          sequencia++;
+        } else {
+          this.exit();
+        }
+      }
+      if (sequencia > 6) {
+        this.callLevel(1, 3)
+      }
+    });
+  }
 
-        case 2:
-          // E, F, G
-          if (sequencia === 1 || sequencia === 4) {
-            if (id === "E") {
-              sequencia++;
-            } else {
-              swal('Errado!', 'Vamos tentar novamente!', 'error');
-              ok = false;
-            }
-          } else if (sequencia === 2 || sequencia === 5) {
-            if (id === "F") {
-              sequencia++;
-            } else {
-              swal('Errado!', 'Vamos tentar novamente!', 'error');
-              ok = false;
-            }
-          } else if (sequencia === 3 || sequencia === 6) {
-            if (id === "G") {
-              sequencia++;
-            } else {
-              swal('Errado!', 'Vamos tentar novamente!', 'error');
-              ok = false;
-            }
-          }
-          if (sequencia > tamanho) {
-            console.log("unsubscribe");
-            this.events.unsubscribe('button:click');
-          }
-          break;
+  async validaM1P3() {
+    let sequencia = 1;
+    console.log("sequencia : " + sequencia);
+    await this.events.subscribe('button:click', (id) => {
+      console.log("validaM1P3");
+      // G, A, G, F, E, C
+      if (sequencia === 1 || sequencia === 7) {
+        if (id === "G") {
+          sequencia++;
+        } else {
+          this.exit();
+        }
+      } else if (sequencia === 2 || sequencia === 8) {
+        if (id === "A") {
+          sequencia++;
+        } else {
+          this.exit();
+        }
+      } else if (sequencia === 3 || sequencia === 9) {
+        if (id === "B") {
+          sequencia++;
+        } else {
+          this.exit();
+        }
+      } else if (sequencia === 4 || sequencia === 10) {
+        if (id === "F") {
+          sequencia++;
+        } else {
+          this.exit();
+        }
+      } else if (sequencia === 5 || sequencia === 11) {
+        if (id === "E") {
+          sequencia++;
+        } else {
+          this.exit();
+        }
+      } else if (sequencia === 6 || sequencia === 12) {
+        if (id === "C") {
+          sequencia++;
+        } else {
+          this.exit();
+        }
+      }
+      if (sequencia > 12) {
+        this.callLevel(1, 4)
+      }
+    });
+  }
 
-        case 3:
-          // G, A, G, F, E, C
-          if (sequencia === 1 || sequencia === 3 || sequencia === 7 || sequencia === 9) {
-            if (id === "G") {
-              sequencia++;
-            } else {
-              swal('Errado!', 'Vamos tentar novamente!', 'error');
-              ok = false;
-            }
-          } else if (sequencia === 2 || sequencia === 8) {
-            if (id === "A") {
-              sequencia++;
-            } else {
-              swal('Errado!', 'Vamos tentar novamente!', 'error');
-              ok = false;
-            }
-          } else if (sequencia === 4 || sequencia === 10) {
-            if (id === "F") {
-              sequencia++;
-            } else {
-              swal('Errado!', 'Vamos tentar novamente!', 'error');
-              ok = false;
-            }
-          } else if (sequencia === 5 || sequencia === 11) {
-            if (id === "E") {
-              sequencia++;
-            } else {
-              swal('Errado!', 'Vamos tentar novamente!', 'error');
-              ok = false;
-            }
-          } else if (sequencia === 6 || sequencia === 12) {
-            if (id === "C") {
-              sequencia++;
-            } else {
-              swal('Errado!', 'Vamos tentar novamente!', 'error');
-              ok = false;
-            }
-          }
-          if (sequencia > tamanho) {
-            console.log("unsubscribe");
-            this.events.unsubscribe('button:click');
-          }
-          break;
-
-        case 4:
-          //G, A, G, F, E, C
-          if (sequencia >= 1 && sequencia <= 6) {
-            if (id === "C") {
-              sequencia++;
-            } else {
-              swal('Errado!', 'Vamos tentar novamente!', 'error');
-              ok = false;
-            }
-          }
-          if (sequencia > tamanho) {
-            console.log("unsubscribe");
-            this.events.unsubscribe('button:click');
-          }
-          break;
+  async validaM1P4() {
+    let sequencia = 1;
+    console.log("sequencia : " + sequencia);
+    await this.events.subscribe('button:click', (id) => {
+      console.log("validaM1P4");
+      //G, A, G, F, E, C
+      if (sequencia === 1 || sequencia === 4) {
+        if (id === "C") {
+          sequencia++;
+        } else {
+          this.exit();
+        }
+      }  else if (sequencia === 2 || sequencia === 5) {
+        if (id === "B") {
+          sequencia++;
+        } else {
+          this.exit();
+        }
+      } else if (sequencia === 3 || sequencia === 6) {
+        if (id === "C2") {
+          sequencia++;
+        } else {
+          this.exit();
+        }
+      }
+      if (sequencia > 6) {
+        swal('Parabéns!', 'Você ganhou!', 'success');
       }
     });
   }
